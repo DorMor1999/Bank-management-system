266d454daab486bcd802ca89632f9dc264f9ed5e
dorm <dorm369258@gmail.com>
2023-07-14 00:01:11 +0300

    organized the files in direcories

--- Bank management system/main.c
+++ Bank management system/main.c
@@ -7,7 +7,7 @@ void main() {
 
     //chack for my self
     AVLNode* root = NULL;
-    /*
+    
     User user1 = { "dor", "mor", "a", 'c' };
     User user2 = { "dor", "mor", "b", 'c' };
     User user3 = { "dor", "mor", "z", 'c' };
@@ -21,7 +21,7 @@ void main() {
     root = insert(root, createUser("dor", "mor", "x", "c"), compare_users_by_email);
 
     inorderTraversal(root, printUser);
-    */
+    
     /*
     root = insert(root, createOperation("send", NULL, NULL), compare_operations_by_id);
     root = insert(root, createOperation("send", NULL, NULL), compare_operations_by_id);
